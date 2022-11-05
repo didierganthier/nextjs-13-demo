c02d40eaa8da8010556fd00a88843701d22ba2a7
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-11-05 09:21:07 -0400

    Add custom 404 page

---
+++ app/todos/[todoId]/not-found.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <div>
+      Whoops we couldn't find the TODO you were looking for
+    </div>
+  )
+}
+
+export default NotFound
